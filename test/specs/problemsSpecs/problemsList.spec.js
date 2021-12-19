879261f5a6af2f1c2acea79803c687a1fa330459
ArtyomSamsonchik <colpachek@gmail.com>
2021-12-20 02:42:10 +0300

    Added 2 tests of problemsList

--- test/specs/problemsSpecs/problemsList.spec.js
+++ test/specs/problemsSpecs/problemsList.spec.js
@@ -3,7 +3,7 @@ const LoginData = require('../../../data/login.data');
 const ProblemsListPage = require('../../pageobjects/ProblemsPages/ProblemsList.page');
 
 describe("ProblemsList page tests", () => {
-  it("Goto problems page", async () => {
+  xit("Goto problems page", async () => {
     let {email, password} = LoginData.artyomCredentials;
     let {pageTitle} = ProblemsListPage;
     await LoginPage.open();
@@ -13,6 +13,32 @@ describe("ProblemsList page tests", () => {
     await expect(pageTitle).toBeExisting();
     await expect(pageTitle).toHaveText("problems");
   });
-});
 
-//npm run cleanWin && 
+  xit("Problems pagination should be by 10", async () => {
+    let {email, password} = LoginData.artyomCredentials;
+    await LoginPage.open();
+    await LoginPage.login(email, password);
+    await ProblemsListPage.openViaSidebar("problems");
+
+    let arr = await ProblemsListPage.problemsArray;
+    await expect(arr).toBeElementsArrayOfSize(10);
+  });
+
+  it("Open first problem page", async () => {
+    let {email, password} = LoginData.artyomCredentials;
+    await LoginPage.open();
+    await LoginPage.login(email, password);
+    await ProblemsListPage.openViaSidebar("problems");
+
+    //let x = await ProblemsListPage.firstProblem.getAttribute("data-id");
+
+    //let problems = await $$('//*[@data-rowindex=0]/../*').getAttribute("role");
+    //let problem = await $$('//*[@data-rowindex=0]/../*').find(el => el.getAttribute("role"));
+
+    let problem = await $('//*[@data-rowindex=0]');
+    await expect (problem).toHaveAttribute("role", "row");
+
+    //let [problem, user] = await ProblemsListPage.problemsArray.$('//a');
+    //await ProblemsListPage.problemsArray[0].$('//a[1]').click();
+  })
+});
